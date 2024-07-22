321a1938f3ca5654acfab7aa9c8202933920c446
Clara Helena <clarahelenasena@gmail.com>
2024-07-22 12:18:00 -0400

    add footer.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from './components/footer'
 import { Header } from './components/header'
 
 function App() {
@@ -5,6 +6,13 @@ function App() {
   return (
     <div className="font-[Poppins] bg-gradient-to-t from-[#38bdf8] to-[#0c4a6e] h-screen">
       <Header/>
+
+      <main>
+        <div>
+        </div>
+      </main>
+
+      <Footer/>
     </div>
   )
 }
---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import { LuHeart } from 'react-icons/lu'
+
+export function Footer() {
+  return (
+    <div className='w-full bg-white flex items-center justify-center'>
+      <p className='p-2'>Made by Clara</p>
+      <LuHeart className='hover:text-[#0ea5e9]'/>
+    </div>
+  )  
+}
