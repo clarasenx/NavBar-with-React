0a1eccbbd01e3e99e2b3d36b754f4fc8de8d0375
Clara Helena <clarahelenasena@gmail.com>
2024-07-23 12:08:46 -0400

    footer update

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,7 +2,7 @@ import { LuHeart } from 'react-icons/lu'
 
 export function Footer() {
   return (
-    <div className='fixed bottom-0 w-full bg-white flex items-center justify-center '>
+    <div className='fixed bottom-0 w-full bg-white flex items-center justify-center h-5 '>
       <p className='p-2'>Made by Clara</p>
       <LuHeart className='hover:text-[#0ea5e9]'/>
     </div>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,13 +3,10 @@ import { Header } from '../components/header'
 export function Home() {
 
   return (
-    <div className="font-[Poppins] bg-gradient-to-t from-[#38bdf8] to-[#0c4a6e] h-full">
+    <div className="font-[Poppins] bg-gradient-to-t from-[#38bdf8] to-[#0c4a6e] relative h-screen">
       <Header/>
 
       <main>
-        <div className='flex items-center justify-center   h-80'>
-          <h1 className='flex text-white'>Home Page</h1>
-        </div>
         <div className='flex items-center justify-center   h-80'>
           <h1 className='flex text-white'>Home Page</h1>
         </div>
