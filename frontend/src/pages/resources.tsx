9bafdbec9fb72faff981003cd439ace5a619d83f
Clara Helena <clarahelenasena@gmail.com>
2024-07-23 12:29:11 -0400

    fix div in main pages

--- frontend/src/pages/resources.tsx
+++ frontend/src/pages/resources.tsx
@@ -2,10 +2,10 @@ import { Header } from '../components/header';
 
 export function Resources() {
   return (
-    <div className="font-[Poppins] bg-gradient-to-t from-[#38bdf8] to-[#0c4a6e] h-screen">
+    <div className="font-[Poppins] bg-gradient-to-t from-[#38bdf8] to-[#0c4a6e] relative h-screen">
       <Header/>
 
-      <div className='flex items-center justify-center my-5   h-20'>
+      <div className='flex items-center justify-center h-80'>
         <h1 className='flex text-white'>Resources Page</h1>
       </div>
     </div>
