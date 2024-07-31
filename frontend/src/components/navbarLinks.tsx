038a992b1ba0c2133db502c35acb74de0c875bbc
Clara Helena <clarahelenasena@gmail.com>
2024-07-31 08:00:52 -0400

    update navBarLinks.tsx

--- frontend/src/components/navbarLinks.tsx
+++ frontend/src/components/navbarLinks.tsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
 
-export const NavBarLinks = () => {
+interface NavBarLinksProps {
+  to: string;
+  label: string;
+}
+
+export const NavBarLinks = ({to, label} : NavBarLinksProps) => {
   return (
     <>
       <li>
-        <Link to="/" className="button-link">
+        <Link to={to} className="button-link">
           <button className="hover:text-[#0c4a6e]">
-            Home
+            {label}
           </button>
         </Link>
       </li>
