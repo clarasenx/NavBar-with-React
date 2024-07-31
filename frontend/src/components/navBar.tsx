c7712a3cf8e75097ebd56366cbbb567a7602c5b9
Clara Helena <clarahelenasena@gmail.com>
2024-07-31 12:54:51 -0400

    update transition on navbar

--- frontend/src/components/navBar.tsx
+++ frontend/src/components/navBar.tsx
@@ -1,5 +1,4 @@
 import { LuAlignJustify, LuGlobe2, LuX } from "react-icons/lu";
-import { CSSTransition } from 'react-transition-group';
 import { NavLinksService } from '../services/navLinkService';
 import { NavBarLinks } from './navbarLinks';
 
@@ -36,49 +35,17 @@ export function NavBar({setIsNavOpen, isNavOpen} : navBarProps) {
           <button className="bg-[#0ea5e9] mr-3 w-24 text-sm text-white px-5 py-1 rounded-full hover:bg-[#0c4a6e]">Sign in</button>
           <button id='navBarButton' onClick={() =>openNavBar()} className='text-[#0c4a6e] flex sm:hidden'>{isNavOpen ? <LuX/> : <LuAlignJustify />}</button>
         </div>
-
-        <CSSTransition
-          in={isNavOpen}
-          timeout={300}
-          classNames="slide-down"
-          unmountOnExit
-        >
-          {/* {isNavBarOpen && ( */}
-            <div className="sm:hidden bg-white h-fit border absolute top-16 left-0 right-0 w-full flex flex-col items-center z-10">
-              <ul className="sm:hidden flex flex-col items-center p-4 gap-8">
-                {NavLinksService.map((link) => (
-                  <NavBarLinks key={link.to} {...link} />
-                ))}
-              </ul>
-
-            </div>
-            </CSSTransition>
-            {/* )
-          } */}
+         <div className={`transition-all duration-300 ease-in-out transform ${isNavOpen ? 'max-h-80 opacity-100' : 'max-h-0 opacity-0'} sm:hidden overflow-hidden bg-white h-fit border absolute top-16 left-0 right-0 w-full flex flex-col items-center`}>
+          {isNavOpen && (
+            <ul className="sm:hidden flex flex-col items-center p-4 gap-8">
+              {NavLinksService.map((link) => (
+                <NavBarLinks key={link.to} {...link} />
+              ))}
+            </ul>
+            )
+          }
+        </div>
       </nav>
     </header>
   )
 }
-
-// const navItem [
-//   "home"
-// ]
-
-// interface asdasd {
-
-// }
-
-// export const nome = () => {
-
-// return (
-//   <>
-//    <li>
-//                   <Link to="/" className="button-link">
-//                     <button className="hover:text-[#0c4a6e]">
-//                      Home
-//                     </button>
-//                   </Link>
-//                 </li>
-//   </>
-// )
-// }
