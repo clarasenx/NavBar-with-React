21a9d30757ddcb126ba3b3cc71765d654bef7a12
Clara Helena <clarahelenasena@gmail.com>
2024-07-31 13:37:23 -0400

    update navbar.tsx

--- frontend/src/components/navBar.tsx
+++ frontend/src/components/navBar.tsx
@@ -36,14 +36,11 @@ export function NavBar({setIsNavOpen, isNavOpen} : navBarProps) {
           <button id='navBarButton' onClick={() =>openNavBar()} className='text-[#0c4a6e] flex sm:hidden'>{isNavOpen ? <LuX/> : <LuAlignJustify />}</button>
         </div>
          <div className={`transition-[max-height] duration-300 ease-in-out ${isNavOpen ? 'max-h-80 opacity-100' : 'max-h-0 opacity-0'} sm:hidden overflow-hidden bg-white h-fit border absolute top-16 left-0 right-0 w-full flex flex-col items-center z-10`}>
-          {isNavOpen && (
             <ul className="sm:hidden flex flex-col items-center p-4 gap-8">
               {NavLinksService.map((link) => (
                 <NavBarLinks key={link.to} {...link} />
               ))}
             </ul>
-            )
-          }
         </div>
       </nav>
     </header>
