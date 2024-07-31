636b6c97699695bb65199570e9106a8db5753013
Clara Helena <clarahelenasena@gmail.com>
2024-07-31 11:49:34 -0400

    update products.tsx

--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -7,8 +7,8 @@ interface ProductsProps {
 export function Products({ isNavOpen }: ProductsProps) {
   return (
     <div className={`font-[Poppins] h-[100dvh] ${isNavOpen ? 'bg-[#0C4A6E]' : 'bg-[#0EA5E9]'}`}>
-      <div className='flex items-center justify-center h-80'>
-        <h1 className='flex text-white'>No products available yet</h1>
+      <div className='flex flex-col items-center'>
+        <h1 className='text-white py-8'>No products available yet</h1>
         <Form/>
       </div>
     </div>
